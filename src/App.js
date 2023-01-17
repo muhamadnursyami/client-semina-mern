4c05a2f90eadd8749323058eed915cf096a75333
Muhamad Nur Syami <nursami385@gmail.com>
2023-01-17 23:34:34 +0700

    Learn Based ReactJs: Part 1 Hook-> useState()

--- src/App.js
+++ src/App.js
@@ -1,79 +1,37 @@
+import { useState } from "react";
 import "./App.css";
-import { Judul } from "./title";
-import { Subtitle } from "./author";
 import Button from "./components/button";
-import Table from "./components/Table";
 
 function App() {
-  // Jika data nya array
-  const data = ["Joko", "Eko", "Yonko"];
-  // Jika data nya Object
-  const users = [
-    {
-      _id: 1,
-      name: "Rikzy",
-      age: 19,
-      status: true,
-    },
-    {
-      _id: 2,
-      name: "Budi",
-      age: 32,
-      status: false,
-    },
-    {
-      _id: 3,
-      name: "Iki",
-      age: 25,
-      status: true,
-    },
-  ];
-
-  const isLogin = true;
+  const [cout, setCout] = useState(0);
+  const [name, setName] = useState("");
+  const [tempatTanggalLahir, setTempatTanggalLahir] = useState("");
   return (
     <>
-      {/* Percobaan 1 */}
-      <ul>
-        <li>Home</li>
-        <li>Users</li>
-        <li>{isLogin ? "Sudah login" : "Login"}</li>
-      </ul>
-      <h3>
-        {/*  mennggunakan component */}
-        <Judul title="Software Engineer" />
-      </h3>
-      <Subtitle name="Muhamad Nur Syami" />
-      <br />
-      <Subtitle />
-      <br />
-      <Button onClick={() => alert("Sudah tersave")}>Save</Button>
-      <br />
-      <br />
-      {/* Data Array dengan Penulisan () tanpa return Cara Kedua*/}
-      <ul>
-        {data.map((user, index) => (
-          <li key={index}> {`Nama Berdasarkan Array: ${user}`}</li>
-        ))}
-      </ul>
+      <h2>Couter :{cout}</h2>
+      <Button onClick={() => setCout(cout + 1)}>Click</Button>
 
-      {/* Data Object  Dengan Penulisan menggunakan {} menggunakan return Cara Pertama*/}
-      <ul>
-        {users.map((user) => {
-          return <li>{`Nama berdasakan object :   ${user.name}`}</li>;
-        })}
-      </ul>
-      {/* Condisional Component */}
-      <ul>
-        {users.map((user) => (
-          <>
-            {user.status && (
-              <li>{`Nama berdasakan object :   ${user.name}`}</li>
-            )}
-          </>
-        ))}
-      </ul>
-
-      <Table users={users} />
+      <form>
+        <br />
+        <label>
+          Name:{" "}
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <label>
+          Tempat Tanggal Lahir:{" "}
+          <input
+            type="text"
+            value={tempatTanggalLahir}
+            onChange={(e) => setTempatTanggalLahir(e.target.value)}
+          />
+        </label>
+      </form>
     </>
   );
 }
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Sbutton({ onClick, children }) {
+export default function Button({ onClick, children }) {
   return <button onClick={onClick}>{children} </button>;
 }
