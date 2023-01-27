b123d379d0207d06cac0ced7de31906e23f63919
Muhamad Nur Syami <nursami385@gmail.com>
2023-01-27 20:15:52 +0700

    learn state 1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Button from "./components/button";
 function App() {
   const [cout, setCout] = useState(0);
   const [name, setName] = useState("");
+  const [usia, setUsia] = useState("");
   const [tempatTanggalLahir, setTempatTanggalLahir] = useState("");
+  const handleClick = () => {
+    setUsia(2022 - tempatTanggalLahir);
+  };
   return (
     <>
       <h2>Couter :{cout}</h2>
@@ -13,24 +17,33 @@ function App() {
 
       <form>
         <br />
-        <label>
-          Name:{" "}
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
+        Name:{" "}
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <br />
         <br />
-        <label>
-          Tempat Tanggal Lahir:{" "}
-          <input
-            type="text"
-            value={tempatTanggalLahir}
-            onChange={(e) => setTempatTanggalLahir(e.target.value)}
-          />
-        </label>
+        Tempat Tanggal Lahir:{" "}
+        <input
+          type="number"
+          value={tempatTanggalLahir}
+          onChange={(e) => setTempatTanggalLahir(e.target.value)}
+        />
+        <br />
+        <br />
+        Umur saya : {usia}
+        <br />
+        <br />
+        <Button
+          onClick={(e) => {
+            handleClick();
+            e.preventDefault();
+          }}
+        >
+          Click
+        </Button>
       </form>
     </>
   );
