97f96171efc9b68e6bcd8da947517733d595534b
Muhamad Nur Syami <nursami385@gmail.com>
2023-02-04 17:32:26 +0700

    Page Dashboard : Navbar, Table, Component Breadcrumb

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-
+import { Container, Nav, Navbar, Table } from "react-bootstrap";
+import SButton from "../../components/Button";
+import SBreadCrumb from "../../components/Breadcrumb";
 export default function Dashboard() {
   // Ngambil token dari localStorage, mengggunakan getItem (KEYnya) saja
   const token = localStorage.getItem("token");
@@ -9,5 +11,53 @@ export default function Dashboard() {
   // Ngecek jika tokennya nggak ada maka redirect ke halaman signin.
 
   if (!token) return <Navigate to="/signin" replace={true} />;
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Semina</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Categories</Nav.Link>
+            <Nav.Link href="#pricing">Talents</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-3">
+        <SBreadCrumb />
+        <SButton>Tambah</SButton>
+
+        <Table className="mt-3" striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan={2}>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+    </>
+  );
 }
